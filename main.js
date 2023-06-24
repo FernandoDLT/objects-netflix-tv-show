17ebab5200e844dfc90b7aa5ad38d8b32a701b22
FernandoDLT <96254499+FernandoDLT@users.noreply.github.com>
2023-06-24 16:13:32 -0600

    Fixed NASA api and added logo

--- main.js
+++ main.js
@@ -11,33 +11,18 @@ class MakeNetFlixTvShow {
     play() {
         console.log('Playing...')
     }
+    pause() {
+        console.log('Pausing play...')
+    }
     stop() {
         console.log('Stopping...')
     }
     saveToFavs() {
         console.log('Saving to favorites list...')
     }
+    deteFromFavs() {
+        console.log('Deleting from favorites list...')
+    }
 }
 let seinfeld = new MakeNetFlixTvShow('Seinfeld', 'Comedy', '99%', 56)
-
-
-
-// class MakeNetflixTvShow{
-//     constructor(title,genre,rating,numOfEp){
-//     this.title = title
-//     this.genre = genre
-//     this.rating = rating
-//     this.numOfEp = numOfEp
-//     }
-//     play() {
-//         console.log('Playing...')
-//     }
-//     stop() {
-//         console.log('Stopping...')
-//     }
-//     saveToList() {
-//         console.log('Save To List')
-//     }
-// }
-
-// let bridgerton = new MakeNetflixTvShow('Bridgerton', 'Period Romance Drama', '99%', 16)
+let peakyBlinders = new MakeNetFlixTvShow('Peaky Blinder', 'Drama Suspense', '100%', 102)
